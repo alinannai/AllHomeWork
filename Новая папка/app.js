52110f50f77bb0de571d85654ea7c3e51f0c7a27
alinannai <doskulovaalina@gmail.com>
2022-09-18 15:22:16 +0600

    delete

--- Новая папка/app.js
+++
@@ -1,8 +0,0 @@
-const mouse = document.querySelector('.mouse');
-mouse.addEventListener("mouseenter", function(event) {
-    console.log('Курсор над элементом');
-});
-mouse.addEventListener("mouseleave", function(event) {
-    console.log('Курсор покинул элемент');
-});
-
