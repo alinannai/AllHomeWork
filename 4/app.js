5c77ee1e0e2f0d05234b97b01397cdf2d2e3c162
alinannai <doskulovaalina@gmail.com>
2022-09-27 19:02:24 +0600

    nmn

--- 4/app.js
+++
@@ -1,4 +0,0 @@
-function hi(){
-    const ww = document.querySelector('.ww');
-    ww.innerHTML = "Goodbye my friend";
-}
--- 5/app.js
+++
@@ -1,6 +0,0 @@
-function on(){
-    document.getElementById('pic').src = 'img/2.png';
-}
-function off(){
-    document.getElementById('pic').src = 'img/1.png';
-}
